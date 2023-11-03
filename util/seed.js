19fac2265c158df15a5efab3b43acdb2af2d1bdd
turiyadev <code@turiya.dev>
2023-11-03 00:54:20 -0600

    Improve seed generator

--- util/seed.js
+++ util/seed.js
@@ -1,5 +1,21 @@
 import { randomBytes } from "node:crypto";
+import { argv } from "node:process";
 
-console.log(randomBytes(32).toString("base64url"));
-console.log(randomBytes(24).toString("base64url"));
-console.log(randomBytes(8).toString("base64url"));
+const argv2 = argv[2];
+const argv3 = argv[3];
+let length = 32;
+let encoding = "hex";
+
+if (argv2) {
+  const i = parseInt(argv2);
+  if (isNaN(i)) {
+    encoding = argv2;
+  } else {
+    length = i;
+  }
+}
+if (argv3) {
+  encoding = argv3;
+}
+
+console.log(randomBytes(length).toString(encoding));
