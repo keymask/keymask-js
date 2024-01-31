dac351745f27b19f47a23cc41e68912657aadbca
turiyadev <code@turiya.dev>
2024-01-31 14:27:02 -0600

    Add StrictKeymask class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export { Keymask, type KeymaskOptions } from "./Keymask";
+export {
+  Keymask,
+  StrictKeymask,
+  type KeymaskData,
+  type KeymaskOptions,
+  type KeymaskType,
+  type KeymaskValue
+} from "./Keymask";
 export { KeymaskEncoder } from "./KeymaskEncoder";
 export { KeymaskGenerator } from "./KeymaskGenerator";
